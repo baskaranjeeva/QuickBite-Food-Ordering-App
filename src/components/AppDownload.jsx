272cab07b688a5c7473ae9db5b8d394b29f63795
baskaranjeeva <baskaran.jeevanandham@gmail.com>
2025-05-21 22:34:17 +0530

    App Download component completed

---
+++ src/components/AppDownload.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+function AppDownload() {
+  return (
+    <>
+      <div className="m-auto mt-[100px] text-4xl text-[max(3vw_20px)] text-center font-bold">
+        <p>
+          For Better Experience Download <br />
+          Quickbite App
+        </p>
+        <div className="flex justify-center gap-3 gap-[max(2vw, 10px)] mt-[40px]">
+          <img
+            className="w-[max(30vw, 120px)] max-w-[180px] cursor-pointer transition-0.5"
+            src={assets.play_store}
+            alt=""
+          />
+          <img
+            className="w-[max(30vw, 120px)] max-w-[180px] cursor-pointer transition-0.5"
+            src={assets.app_store}
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AppDownload;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import AppDownload from "../../components/AppDownload";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header />
       <ExploreMenu />
       <FoodDisplay />
+      <AppDownload />
     </div>
   );
 }
